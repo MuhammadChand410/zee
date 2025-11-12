0ce7a169a4a47ea9cbe5236fac803bab437c55fa
MuhammadChand410 <muhammadchand00001@gmail.com>
2025-11-12 14:34:02 +0500

    add imoact ma max width

--- src/pages/product/impact-product.jsx
+++ src/pages/product/impact-product.jsx
@@ -28,20 +28,19 @@ export default function ImpactProduct() {
     return (
         <section className="md:py-20 py-8 bg-[#0B0B0B]">
             <div className="container">
-                <div className="flex md:flex-row flex-col gap-8 md:gap-20">
+                <div className="grid grid-cols-1 md:[grid-template-columns:minmax(100px,502px)_minmax(100px,659px)] gap-8 md:gap-14">
                     <div>
-                        <img src={ImpactImage} alt="impact-image" width={502} height={361} loading="lazy" />
+                        <img src={ImpactImage} className="w-full" alt="impact-image" width={502} height={361} loading="lazy" />
                     </div>
                     <div>
-                        <div className="max-w-[640px]">
-                            <HeroContant subheading={`IMPACT`} classNameSub={`md:!pb-8 !pb-4 !text-start`} heading={`Transform Vision into Measurable Product Success`} classNamehead={`md:!leading-[48px] !leading-[40px] !text-start md:!text-[40px] !text-[28px]`} desc={`Our Product Strategy Consulting bridges the gap between design, business, and technology. We define clear goals, align user needs with business outcomes, and guide your product from concept to market success.`} classNameDesc={`!font-[Plus Jakarta Sans] !leading-[21px] !pb-8 !text-start`} />
-                            <div className="grid md:grid-cols-2 grid-cols-1 ">
+                        <div className="">
+                            <HeroContant subheading={`IMPACT`} classNameSub={`!pb-6 !text-start`} heading={`Transform Vision into Measurable Product Success`} classNamehead={`md:!leading-[48px] !leading-[40px] !text-start md:!text-[34px] !text-[28px]`} desc={`Our Product Strategy Consulting bridges the gap between design, business, and technology. We define clear goals, align user needs with business outcomes, and guide your product from concept to market success.`} classNameDesc={`!font-[Plus Jakarta Sans] !leading-[21px] !pb-8 !text-start`} />
+                            <div className="grid grid-cols-1 md:[grid-template-columns:minmax(100px,270px)_minmax(100px,328px)] gap-2">
                                 {IMPACT_CARD.map((card =>
-                                    <div className="flex gap-3 pb-4" key={card.id}>
+                                    <div className="flex gap-3 pb-2" key={card.id}>
                                         <ImpactIcon />
                                         <h2 className="text-sm font-medium leading-[21px] text-[#FFFFFF]">{card.title}</h2>
                                     </div>
-
                                 ))}
                             </div>
                         </div>
